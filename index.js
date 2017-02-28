74174a965bfce675cd4a2b337f7741f2da356d64
Tyler Murphy <tmurphy@roirevolution.com>
2017-02-27 20:43:16 -0500

    Initial commit. Working with tests.

---
+++ index.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto')
+
+const defaultCharacters = 10
+const hashAlgorithm = 'sha256'
+const fields = [
+    'get',
+    'set',
+    'has',
+    'delete'
+]
+
+module.exports = function wrap(map, maxLength = defaultCharacters) {
+    fields.forEach(field => {
+        const prototype = Reflect.getPrototypeOf(map)
+        const descriptor =
+            Reflect.getOwnPropertyDescriptor(map, field) ||
+            Reflect.getOwnPropertyDescriptor(prototype, field)
+
+        Reflect.defineProperty(map, field, Object.assign(descriptor, {
+            value: new Proxy(map[field], {
+                apply: function(defaultFunction, map, args) {
+                    if (typeof args[0] === 'string' && args[0].length > maxLength) {
+                        args[0] = encode(args[0]).slice(0, maxLength)
+                    }
+
+                    return Reflect.apply(defaultFunction, map, args)
+                }
+            })
+        }))
+    })
+
+    return map
+}
+
+function encode(string) {
+    return crypto
+    .createHash(hashAlgorithm)
+    .update(string)
+    .digest('base64')
+}
---
+++ test.js
@@ -0,0 +1,88 @@
+const test = require('tape')
+const wrap = require('./index.js')
+
+test('no hashing', t => {
+    const maxLength = 10
+    const map = wrap(new Map(), maxLength)
+    const entries = [
+        ['a', 1],
+        ['1234567890', 10]
+    ]
+
+    entries.forEach(([key, value]) => {
+        map.set(key, value)
+
+        t.equal(map.keys().next().value, key)
+        t.equal(map.get(key), value)
+        t.assert(map.has(key))
+        t.assert(map.delete(key))
+        t.assert(!map.has(key))
+    })
+
+    t.end()
+})
+
+test('hashing', t => {
+    const maxLength = 3
+    const map = wrap(new Map(), maxLength)
+    const entries = [
+        ['abcde', 123],
+        ['1234567890', 10]
+    ]
+
+    entries.forEach(([key, value]) => {
+        map.set(key, value)
+
+        t.notEqual(map.keys().next().value, key)
+        t.equal(map.get(key), value)
+        t.assert(map.has(key))
+        t.assert(map.delete(key))
+        t.assert(!map.has(key))
+    })
+
+    t.end()
+})
+
+test('custom maps', t => {
+    const noop = () => {}
+    const map = wrap({
+        get: noop,
+        set: noop,
+        has: noop,
+        delete: noop
+    })
+
+    'get set has delete'.split(' ').forEach(name => {
+        t.equal(typeof map[name], 'function')
+    })
+
+    t.end()
+})
+
+test('requires get, set, has, and delete', t => {
+    t.throws(() => wrap({}))
+
+    t.end()
+})
+
+test('requires get, set, has, and delete to be functions', t => {
+    const get = set = has = () => {}
+
+    t.throws(() => wrap({ get, set, has, delete: null }))
+
+    t.end()
+})
+
+test('non-string keys', t => {
+    const map = wrap(new Map())
+    const key = ['some', 'data']
+    const value = []
+
+    map.set(key, value)
+    t.assert(map.has(key))
+    t.equal(map.get(key), value)
+    t.assert(map.delete(key))
+    t.assert(!map.has(key))
+
+    t.end()
+})
